9b29af54acbfbe7ad1642d624ddd25af1a076d8d
高松幸平 <gaoxin.xixxix@gmail.com>
2015-07-09 02:34:27 +0900

    mysql用拡張クラスとりあえず完成

---
+++ sqlInterface.php
@@ -0,0 +1,185 @@
+<?php
+
+class sqlInterface extends mysqli{
+    private $db;
+    /**
+     * __construct description
+     * @param string $host     host name you're going to connect
+     * @param string $user     user name you're going to log in with
+     * @param string $password pass word you're going to log in with
+     * @param string $database database name you're going to connect
+     */
+   function __construct($host,$user,$password,$database) {
+        parent::__construct($host,$user,$password,$database);
+    }
+
+   /**
+    * _query execute a query
+    * @param  string $sql    sql string
+    * @param  boolean $if_one indicate if the result is one record
+    * @return array  $result result
+    */
+   private function _query($sql,$if_one){
+        $result = parent::query($sql);
+        if(is_bool($result)){
+            return $result;
+        }
+        if($if_one){
+            return $result->fetch_assoc();
+        }else{
+            $tmp = array();
+            while($row = $result->fetch_assoc()){
+                $tmp[] = $row;
+            }
+            return $tmp;
+        }
+
+    }
+   /**
+    * select execute selection from the db
+    * @param  string       $table  table name
+    * @param  string|array $where  select condtion for the selection expressed by string or associative array
+    * @param  string|array $sort   order condition expressed by string or associative array
+    * @param  string       $fields field names you want to pick up
+    * @param  boolean      $if_one indicate if the result is one record
+    * @return array                result
+    */
+    public function select($table,$where,$sort,$if_one=false,$cols = "*"){
+        if($where){
+            $wherestr = " where";
+            if(is_array($where)){
+                $index = 0;
+                foreach($where as $key => $val){
+                    if($index != 0){
+                        $wherestr .= " AND";
+                    }
+                    if(is_string($val)){
+                        $wherestr .= $key."='".$val."'";
+                    }else{
+                        $wherestr .= $key."=".$val;;
+                    }
+                    $index++;
+                }
+            }else{
+                $wherestr .= $where;
+            }
+        }
+        if($sort){
+            $sortstr = " ORDER BY";
+            if(is_array($sort)){
+                $index = 0;
+                foreach($sort as $key => $val){
+                    if($index != 0){
+                        $sortstr .= " AND";
+                    }
+                    $sortstr .= $key."=".$val;
+                    $index++;
+                }
+            }else{
+                $sortstr .= $sort;
+            }
+        }
+        $sql = "SELECT ".$cols." FROM ".$table.$where.$sort.";";
+        return $this->_query($sql,$if_one);
+    }
+    public function update($table,$where,$data){
+        if($where){
+            $wherestr = " where ";
+            if(is_array($where)){
+                $index = 0;
+                foreach($where as $key => $val){
+                    if($index != 0){
+                        $wherestr .= " AND";
+                    }
+                    if(is_string($val)){
+                        $wherestr .= $key."='".$val."'";
+                    }else{
+                        $wherestr .= $key."=".$val;;
+                    }
+                    $index++;
+                }
+            }else{
+                $wherestr .= $where;
+            }
+        }else{
+            return false;
+        }
+        if($data){
+            $datastr = " SET ";
+            if(is_array($data)){
+                $index = 0;
+                foreach($data as $key => $val){
+                    if($index != 0){
+                        $datastr .= " ,";
+                    }
+                    if(is_string($val)){
+                        $datastr .= $key."='".$val."'";
+                    }else{
+                        $datastr .= $key."=".$val;
+                    }
+                    $index++;
+                }
+            }else{
+                $datastr .= $data;
+            }
+        }else{
+            return false;
+        }
+        $sql = "UPDATE ".$table.$datastr.$wherestr.";";
+        $if_one = true;
+        return $this->_query($sql,$if_one);
+   }
+   public function insert($table,$data){
+        if($data){
+            $cols = " (";
+            $vals = " (";
+            $index = 0;
+            foreach($data as $key => $val){
+                if($index != 0){
+                    $cols .= " ,";
+                    $vals .= " ,";
+                }
+                $cols .= $key;
+                if(is_string($val)){
+                    $vals .= "'".$val."'";
+                }else{
+                    $vals .=$val;
+                }
+                $index++;
+            }
+            $cols .= ") ";
+            $vals .= ");";
+        }else{
+            return false;
+        }
+        $sql = "INSERT INTO ".$table.$cols." VALUES ".$vals;
+        $if_one = true;
+        return $this->_query($sql,$if_one);
+   }
+
+   public function delete($table,$where){
+        if($where){
+            $wherestr = " where ";
+            if(is_array($where)){
+                $index = 0;
+                foreach($where as $key => $val){
+                    if($index != 0){
+                        $wherestr .= " AND";
+                    }
+                    if(is_string($val)){
+                        $wherestr .= $key."='".$val."'";
+                    }else{
+                        $wherestr .= $key."=".$val;;
+                    }
+                    $index++;
+                }
+            }else{
+                $wherestr .= $where;
+            }
+        }
+        $sql = "DELETE FROM ".$table.$wherestr;
+        $if_one = true;
+        return $this->_query($sql,$if_one);
+   }
+
+}
